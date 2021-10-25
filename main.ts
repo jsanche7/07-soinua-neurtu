cc8f77cc3eb88d5f41b1fa00b1cb320ad9a62be5
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-10-25 13:35:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ basic.forever(function () {
     if (input.soundLevel() < 10) {
         basic.showIcon(IconNames.Happy)
     } else {
-        basic.showIcon(IconNames.Sad)
+        basic.showIcon(IconNames.Tortoise)
     }
 })
